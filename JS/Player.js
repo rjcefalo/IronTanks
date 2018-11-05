d1a02661a20c72fd825cf93d73acd39d2be31195
Ricardo Cefalo <ricardo.cefalo@gmail.com>
2018-11-05 15:19:44 +0100

    Player Class and Collition Function

---
+++ JS/Player.js
@@ -0,0 +1,26 @@
+export default class Player {
+  constructor(id, playerType, player) {
+    this.canvas = document.getElementById(id);
+    this.ctx = this.canvas.getContext('2d');
+    this.modelo = new Image();
+    this.modelo.src = `../images/Vector/${player}.svg`;
+    if (playerType == 1) {
+      this.playerPosX = Math.random() * ((this.canvas.width - 45) / 2);
+    } else {
+      this.playerPosX = Math.random() * ((this.canvas.width - this.modelo.width) - (this.canvas.width / 2)) + (this.canvas.width / 2);
+      // this.ctx.translate(width, 0);
+      // this.ctx.scale(-1, 1);
+    }
+    this.playerPosY = this.canvas.height / 2;
+  }
+
+  draw() {
+    this.ctx.drawImage(
+      this.modelo,
+      this.playerPosX,
+      this.playerPosY,
+      this.modelo.width * 2,
+      this.modelo.height * 2,
+    );
+  }
+}
--- JS/script.js
+++ JS/script.js
@@ -1,15 +1,20 @@
+/* eslint-disable import/extensions */
 import Background from './Background.js';
 import GroundGenerator from './GroundGenerator.js';
+import Player from './Player.js';
 
 window.onload = function () {
   $('#game-board').html('<canvas id="ironcanvas" height="500" width="800"></canvas>');
   const background = new Background('ironcanvas');
   const ground = new GroundGenerator('ironcanvas');
+  const players = [new Player('ironcanvas', 1, 'greenTank1'), new Player('ironcanvas', 2, 'blueTank1')];
   let counter = 0;
 
   const intervalo = setInterval(() => {
     background.draw();
     ground.drawGround();
+    players[0].draw();
+    players[1].draw();
     counter++;
     if (counter == 50) clearInterval(intervalo);
   }, 1000 / 60);
